46981efa0f1da03fdded4aa6da45d5ac1959159d
Saumya Saksena <saumya.saksena@gitlab.navya.tech>
2025-10-22 15:14:40 +0200

    Remove everything

--- models/pretrained/resnet_v2_101/download.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-wget http://download.tensorflow.org/models/resnet_v2_101_2017_04_14.tar.gz
-tar -xvf resnet_v2_101_2017_04_14.tar.gz
-rm resnet_v2_101_2017_04_14.tar.gz
--- tf_pose/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from tf_pose.runner import infer, Estimator, get_estimator
